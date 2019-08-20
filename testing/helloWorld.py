d1c1503be4f57e2a87cadaceda5e395936e5ec54
William Goble <wg13b@my.fsu.edu>
2019-08-20 14:44:33 -0400

    Complete welcome message for Admin

--- testing/helloWorld.py
+++ testing/helloWorld.py
@@ -2,6 +2,6 @@
 print("What is your name?")
 name = input()
 if (name == "William"):
-	print("Welcome Admin")
+	print("Welcome Admin, and welcome to GitKraken")
 else:
 	print("Hello " + name + ", welcome to GitKraken")
